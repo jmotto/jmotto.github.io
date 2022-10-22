38196ad7f02646e02ab5cbd9ab9911c755041e35
Jamie Otto <jmotto31@gmail.com>
2022-10-22 00:05:20 -0700

    added layout

---
+++ src/components/Portfolio.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+export const Portfolio = () => {
+  return (
+    <ImageList sx={{ width: 500, height: 500 }}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            position="below"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  
+];
+
+
+// import * as React from 'react';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+
+
+// export const Portfolio = () => {
+//   return (
+//     <Card sx={{ maxWidth: 345 }}>
+//       <CardMedia
+//         component="img"
+//         height="140"
+//         image="src/images/got-milk-image.jpg"
+//         alt="got-milk-image"
+//       />
+//       <CardContent>
+//         <Typography gutterBottom variant="h5" component="div">
+//           Got Milk Grocery
+//         </Typography>
+//         <Typography variant="body2" color="text.secondary">
+//           Never forget your list with this full-stack grocery app, Got Milk Grocery.
+//         </Typography>
+//       </CardContent>
+//       <CardActions>
+//         <Button size="small">Share</Button>
+//         <Button size="small">Learn More</Button>
+//       </CardActions>
+//     </Card>
+//   );
+// }
